0c0d8000c2e94480e84ae20e82695b41ebb3b926
b4lk0n <b4lk0n@gmail.com>
2020-02-20 18:21:41 +0200

    add firebase to client package

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,17 @@ import React from 'react';
 import { render } from 'react-dom';
 import ApolloClient, { gql } from 'apollo-boost';
 import { ApolloProvider, useMutation } from '@apollo/react-hooks';
+import { initializeApp } from 'firebase/app';
+
+const app = initializeApp({
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+});
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000'
