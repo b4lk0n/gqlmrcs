c8297c35c2778074ea850588e7d36091ac560807
b4lk0n <b4lk0n@gmail.com>
2020-02-20 18:23:59 +0200

    remove unused var

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,7 +4,7 @@ import ApolloClient, { gql } from 'apollo-boost';
 import { ApolloProvider, useMutation } from '@apollo/react-hooks';
 import { initializeApp } from 'firebase/app';
 
-const app = initializeApp({
+initializeApp({
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
